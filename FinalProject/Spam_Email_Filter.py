927007f2c575e08e3816520f559601d3b3759a9c
TheRealMilesLee <grandtheftauto710@outlook.com>
2023-03-25 22:20:26 -0500

    update

--- FinalProject/Spam_Email_Filter.py
+++ FinalProject/Spam_Email_Filter.py
@@ -1,39 +1,15 @@
-# import tensorflow as tf
-# from tensorflow import keras
-# from tensorflow.keras import layers
-import os
-import glob
-data_path = 'trec05p-1'
-emails = []
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
-for root, dirs, files in os.walk(data_path):
-    for file in glob.glob(os.path.join(root, '*.txt')):
-        with open(file, 'r', encoding='ISO-8859-1') as f:
-            text = f.read()
-            emails.append(text)
 
-print(emails)
-print('Number of emails:', len(emails))
+# Load the CSV file into a pandas dataframe
+data = pd.read_csv('spam.csv', encoding='utf-8')
 
-# tokenizer = keras.preprocessing.text.Tokenizer(num_words=10000)
-# tokenizer.fit_on_texts(train_data['text'])
+# Extract the message and label columns
+messages = data['v2'].values
+labels = data['v1'].values
 
-# train_sequences = tokenizer.texts_to_sequences(train_data['text'])
-# test_sequences = tokenizer.texts_to_sequences(test_data['text'])
-# train_data = keras.preprocessing.sequence.pad_sequences(
-#     train_sequences, maxlen=100)
-# test_data = keras.preprocessing.sequence.pad_sequences(
-#     test_sequences, maxlen=100)
+# Split the data into training and validation sets
+train_messages, train_labels = train_test_split(messages, labels)
 
-# model = keras.Sequential([
-#     layers.Embedding(10000, 16, input_length=100),
-#     layers.GlobalAveragePooling1D(),
-#     layers.Dense(16, activation='relu'),
-#     layers.Dense(1, activation='sigmoid')])
-
-# model.compile(optimizer='adam', loss='binary_crossentropy',
-#               metrics=['accuracy'])
-# model.fit(train_data, train_labels, epochs=10,
-#           batch_size=32, validation_split=0.2)
-# results = model.evaluate(test_data, test_labels)
-# print('Test accuracy:', results[1])
+print(train_labels)
