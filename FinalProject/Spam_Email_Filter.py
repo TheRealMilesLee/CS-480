65b46255651f257d5c61392eab6af3bd1e2c9c5b
TheRealMilesLee <grandtheftauto710@outlook.com>
2023-03-26 14:19:23 -0500

    Dataset Cleanup

--- FinalProject/Spam_Email_Filter.py
+++ FinalProject/Spam_Email_Filter.py
@@ -1,9 +1,18 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
 
-
+FilePath = 'spam.csv'
+processedFilePath = 'Processed.csv'
+with open(FilePath, 'rb') as csv_in:
+    with open(processedFilePath, "w", encoding="utf-8") as csv_temp:
+        for line in csv_in:
+            if not line:
+                break
+            else:
+                line = line.decode("utf-8", "ignore")
+                csv_temp.write(str(line).rstrip() + '\n')
 # Load the CSV file into a pandas dataframe
-data = pd.read_csv('spam.csv', encoding='utf-8')
+data = pd.read_csv(processedFilePath, encoding='utf-8')
 
 # Extract the message and label columns
 messages = data['v2'].values
